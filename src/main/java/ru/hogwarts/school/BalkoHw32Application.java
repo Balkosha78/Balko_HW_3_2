44d6eab290b74b617b98d890c5572e83f30e503b
Balkosha78 <balko-aleksey@yandex.ru>
2023-07-13 15:58:57 +0700

    initial commit

---
+++ src/main/java/ru/hogwarts/school/BalkoHw32Application.java
@@ -0,0 +1,13 @@
+package ru.hogwarts.school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BalkoHw32Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BalkoHw32Application.class, args);
+    }
+
+}
